fcbba545440928285ac0e2179fd8ff0afb4e8d52
Justin Robles <Justin Robles>
2017-10-28 13:02:29 -0700

    Initial Commit

---
+++ Assets/CollisionTester.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionTester : MonoBehaviour {
+    private Rigidbody rb;
+    Vector3 _myPriorPosition;
+    Vector3 _myVelocityVector;
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.name == "Ball")
+        {
+            Debug.Log("Before Ball vel: " + col.gameObject.GetComponent<Rigidbody>().velocity);
+            Debug.Log("Before Racket vel: " + (_myVelocityVector));
+            Debug.Log("test: " + (_myVelocityVector.x + col.gameObject.GetComponent<Rigidbody>().velocity.x));
+            col.gameObject.GetComponent<Rigidbody>().velocity += _myVelocityVector;
+            Debug.Log("After " + col.gameObject.GetComponent<Rigidbody>().velocity);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(gameObject.name + " was triggered by " + other.gameObject.name);
+    }
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        _myVelocityVector = (transform.position - _myPriorPosition) / Time.deltaTime;
+        _myPriorPosition = transform.position;
+    }
+}
---
+++ Assets/ballCollision.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballCollision : MonoBehaviour {
+    private Rigidbody rb;
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Table")
+        {
+            rb.AddForce(0, 2, 0);
+        }
+    }
+
+        // Use this for initialization
+        void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rb = GetComponent<Rigidbody>();
+    }
+}
